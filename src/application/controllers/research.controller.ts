eb458798b3f6d2822475a6b4d25b71fdf352c17a
Nahuel Bustamante Murua <nahuel.bustamante@gmail.com>
2020-07-13 21:16:29 -0300

    fix param in research controller

--- src/application/controllers/research.controller.ts
+++ src/application/controllers/research.controller.ts
@@ -4,9 +4,8 @@ import {
 } from "../../domain/research/research.repository.ts";
 
 async function getResearchByUserId(req: Request, res: Response) {
-  let planetID = req.params.planet_id;
   let userID = req.params.user_id;
-  let research = await getResearch(planetID, userID);
+  let research = await getResearch(userID);
   res.status(200).send(research[0]);
 }
 
